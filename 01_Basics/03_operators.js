534271b66c83e3087e7bb1248b588b6c50a0b752
ayush <ayush91003@gmail.com>
2025-06-09 19:44:43 +0530

    assignment operators

--- 01_Basics/03_operators.js
+++ 01_Basics/03_operators.js
@@ -6,3 +6,13 @@ console.log("Multiplication:", a * b); // 30
 console.log("Division:", a / b);       // 3.333...
 console.log("Modulus:", a % b);        // 1
 console.log("Exponentiation:", a ** b);// 1000
+
+
+// ===== Assignment Operators =====
+let x = 5;
+x += 2;  // x = 7
+x *= 3;  // x = 21
+x -= 1;  // x = 20
+x /= 2;  // x = 10
+x %= 3;  // x = 1
+console.log("Final value of x:", x);
